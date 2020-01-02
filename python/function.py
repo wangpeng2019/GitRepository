d932b74889d3d5846119b76b45c6ff0b2da59cdc
wangpi <825015485@qq.com>
2020-01-02 23:04:43 +0800

    实参的多种传递方法

--- python/function.py
+++ python/function.py
@@ -13,5 +13,6 @@ def describe_pet(pet_name,animal_type='dog'):
 	print("\nI have a " + animal_type + ".")
 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 	
-describe_pet('harry')
+describe_pet('harry','hamster')
 describe_pet(pet_name='harry',animal_type='hamster')
+describe_pet(animal_type='hamster',pet_name='harry')

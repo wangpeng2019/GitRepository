bbff224e59f290e26c9f8b2756b68446c582fa57
wangpi <825015485@qq.com>
2020-01-02 23:00:20 +0800

    形参提供默认值

--- python/function.py
+++ python/function.py
@@ -8,10 +8,10 @@ greet_user('wangpeng')
 '''
 
 #pets.py
-def describe_pet(animal_type,pet_name):
+def describe_pet(pet_name,animal_type='dog'):
 	"""显示宠物信息"""
 	print("\nI have a " + animal_type + ".")
 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 	
-describe_pet('hamster','harry')
+describe_pet('harry')
 describe_pet(pet_name='harry',animal_type='hamster')

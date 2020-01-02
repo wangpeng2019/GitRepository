c8e2a5832a7203c5ce346442b505dfc968a7d3e5
wangpi <825015485@qq.com>
2020-01-02 22:39:32 +0800

    宏物传1次位置实参

--- python/function.py
+++ python/function.py
@@ -1,6 +1,16 @@
+'''
 #greeter.py
 def greet_user(username):
 	#显示打印信息
 	print("hello,"+username.title()+"!")
 	
 greet_user('wangpeng')
+'''
+
+#pets.py
+def describe_pet(animal_type,pet_name):
+	"""显示宠物信息"""
+	print("\nI have a " + animal_type + ".")
+	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+	
+describe_pet('hamster','harry')

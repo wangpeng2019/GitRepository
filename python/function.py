f4f424b2cc8b8a683eea03a702dd290e7043d03b
wangpi <825015485@qq.com>
2020-01-02 22:51:18 +0800

    关键字传实参

--- python/function.py
+++ python/function.py
@@ -14,4 +14,4 @@ def describe_pet(animal_type,pet_name):
 	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 	
 describe_pet('hamster','harry')
-describe_pet('dog','willer')
+describe_pet(pet_name='harry',animal_type='hamster')

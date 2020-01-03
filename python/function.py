60fd92a51703bd3e81d368cc8b2d537607710dd4
wangpi <825015485@qq.com>
2020-01-03 20:45:28 +0800

    返回值使用01版

--- python/function.py
+++ python/function.py
@@ -5,7 +5,7 @@ def greet_user(username):
 	print("hello,"+username.title()+"!")
 	
 greet_user('wangpeng')
-'''
+
 
 #pets.py
 def describe_pet(pet_name,animal_type='dog'):
@@ -16,3 +16,13 @@ def describe_pet(pet_name,animal_type='dog'):
 describe_pet('harry','hamster')
 describe_pet(pet_name='harry',animal_type='hamster')
 describe_pet(animal_type='hamster',pet_name='harry')
+'''
+#formatted_name.py
+#返回值
+def get_formatted_name(first_name,last_name):
+	'''返回整洁的姓名'''
+	full_name=first_name + ' ' + last_name
+	return full_name.title()
+
+musician=get_formatted_name('jimi','hedrix')
+print(musician)

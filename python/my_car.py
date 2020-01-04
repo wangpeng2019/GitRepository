47a37334a1a512dc1a123e510706965e1e01b466
wangpi <825015485@qq.com>
2020-01-04 23:12:04 +0800

    调用别的类模块

---
+++ python/my_car.py
@@ -0,0 +1,9 @@
+from car import Car
+from electric_car import ElectriCar
+
+my_beetbl=Car('volkswagen','beetlw',2016)
+print(my_beetbl.get_descriptive_name())
+
+my_tesla=ElectriCar('tesla','modes',2016)
+print(my_tesla.get_descriptive_name())
+
